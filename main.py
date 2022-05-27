1ab3db14d527e47b8435fc82472429cd43bb7a19
rkubik <bara14@op.pl>
2022-05-27 11:12:36 +0200

    random method changed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from random import random
+import random
 
 from ship import Ship
 def create_2d_array(x,y):
@@ -9,13 +9,20 @@ def print_array(array):
     for row in array:
      print(row)
 
+def put_ship_on_matrix(ship, array):
+    x_point = random.randint(0, 9)
+    y_point = random.randint(0, 9)
+    array[x_point][y_point] = 1
+    print_array(array)
+
+
 array = create_2d_array(10,10)
-print_array(array)
+#print_array(array)
+
+
 
 ship1 = Ship(1, "mama")
 types_of_ships = {"onePole" : 4, "twoPole" : 3, "threePole" :3, "fourPole" : 1}
 
-def put_ship_on_matrix(ship):
-    start_x_point = random(0,9)
-    start_y_point = random(0,9)
+put_ship_on_matrix(ship1, array)
 
